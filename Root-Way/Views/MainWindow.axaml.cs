8ae4d9d95221f978657bf2f6c62721f73f598751
Mina-Village <cosasparaclases@gmail.com>
2023-05-11 09:40:14 +0200

    Se supone que ahora la pantalla se puede poner en completa y se centra.

--- Root-Way/Views/MainWindow.axaml.cs
+++ Root-Way/Views/MainWindow.axaml.cs
@@ -1,4 +1,8 @@
+using System.Runtime.InteropServices;
 using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+
 
 namespace Root_Way.Views;
 
@@ -7,5 +11,36 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+    
+    private void pnlControlBar_PointerPressed(object sender, PointerPressedEventArgs e)
+    {
+        if (e.GetCurrentPoint(this).Properties.IsLeftButtonPressed)
+        {
+            //mueve la ventana
+            BeginMoveDrag(e);
+        }
+    }
+    private void pnlControlBar_PointerEnter(object sender, PointerEventArgs e)
+    {
+        // obtiene la altura de la pantalla principal
+        this.MaxHeight = Screens.Primary.WorkingArea.Height;
+    }
+
+    private void btnClose_Click(object sender, RoutedEventArgs e)
+    {
+        //cierra la ventana actual
+        this.Close();
+    }
+    
+    private void btnMinimize_Click(object sender, RoutedEventArgs e)
+    {
+        this.WindowState = WindowState.Minimized;
+    }
+    private void btnMaximize_Click(object sender, RoutedEventArgs e)
+    {
+        if (this.WindowState == WindowState.Normal)
+            this.WindowState = WindowState.Maximized;
+        else this.WindowState = WindowState.Normal;
     }
 }
